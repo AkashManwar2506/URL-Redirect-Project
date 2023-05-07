7e05bea063ba86cec092692eb87e9034b37f9bfd
Akash Manwar <akashmanwar@Akashs-MacBook-Air.local>
2023-05-07 22:15:18 +0530

    Day 4

---
+++ models/user.models.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    logo: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {type: String, enum: ["User", "Admin"], default: "User"}
+});
+
+
+const UserModel = mongoose.model('user', userSchema);
+
+module.exports={UserModel}
---
+++ routes/url.route.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const urlRouter = express.Router()
+const url = require("../models/urlmodel");
+const authenticate = require("../middelwares/authentication");
+
+urlRouter.post("/assign", authenticate, async(req, res)=>{
+    try {
+        const {longurl} = req.body;
+        if (!longurl) return res.json({msg: "Please Provide URL"})
+        
+        const newurl = new url({longurl})
+        newurl.author = res.locals.userId
+        // console.log(res.locals)
+        await newurl.save()
+        res.send(newurl)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+urlRouter.get("/", async(req, res)=>{
+    try {
+        const totalUrls = await url.find({})
+        res.json(totalUrls)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
+
+module.exports = urlRouter
