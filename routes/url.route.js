0c04622ab1224d9e3a4bcc10802b3ed9a59820db
Akash Manwar <akashmanwar@Akashs-MacBook-Air.local>
2023-05-08 16:42:50 +0530

    removed authentication

--- routes/url.route.js
+++ routes/url.route.js
@@ -3,13 +3,13 @@ const urlRouter = express.Router()
 const url = require("../models/urlmodel");
 const authenticate = require("../middelwares/authentication");
 
-urlRouter.post("/assign", authenticate, async(req, res)=>{
+urlRouter.post("/assign", async(req, res)=>{
     try {
-        const {longurl} = req.body;
+        const {longurl, id} = req.body;
         if (!longurl) return res.json({msg: "Please Provide URL"})
         
-        const newurl = new url({longurl})
-        newurl.author = res.locals.userId
+        const newurl = new url({longurl, author: id})
+        
         // console.log(res.locals)
         await newurl.save()
         res.send(newurl)
